52fd1847b20c5f8c3af391820bdf1b54ccbf89fe
Zyad-25 <01556746404ziad@gmail.com>
2024-11-14 17:28:13 +0200

    sec commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="">
     <div className=' min-h-screen main-color flex justify-center items-center '>
         <div className=" w-full flex justify-center items-center flex-col">
-          <img src="src/assets/avataaars.svg" className='w-[230px]' alt="" />
+          <img src="/avataaars.svg" className='w-[230px]' alt="" />
           <h1 className='uppercase text-4xl font-bold text-white pt-8'>start framework</h1>
           <div className="flex justify-between px-12 pt-5">
           <div className="border-b-4 me-3 border-white h-3 w-[100px]"></div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,39 +18,39 @@ export default function Portfolio() {
             </div>
             <div className=' max-w-screen-xl mx-auto m-5 flex flex-col gap-10'>
                 <div className=" container mx-auto grid grid-cols-12 gap-10 p-4">
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/poert1.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
-                        <img src="src/assets/poert1.png" className='rounded-lg w-full' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/poert1.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
+                        <img src="/poert1.png" className='rounded-lg w-full' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
                     </div>
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/port2.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
-                        <img src="src/assets/port2.png" className='rounded-lg' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/port2.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
+                        <img src="/port2.png" className='rounded-lg' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
                     </div>
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/port3.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4  rounded-lg car ">
-                        <img src="src/assets/port3.png" className='rounded-lg' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/port3.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4  rounded-lg car ">
+                        <img src="/port3.png" className='rounded-lg' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
                     </div>
 
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/poert1.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4  rounded-lg car ">
-                        <img src="src/assets/poert1.png" className='rounded-lg' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/poert1.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4  rounded-lg car ">
+                        <img src="/poert1.png" className='rounded-lg' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
                     </div>
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/port2.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
-                        <img src="src/assets/port2.png" className='rounded-lg' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/port2.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
+                        <img src="/port2.png" className='rounded-lg' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
                     </div>
-                    <div onClick={() => { setOpenModal(true), setimg('src/assets/port3.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
-                        <img src="src/assets/port3.png" className='rounded-lg' alt="" />
+                    <div onClick={() => { setOpenModal(true), setimg('/port3.png') }} className="relative max-sm:col-span-12 max-md:col-span-6 col-span-4 rounded-lg car ">
+                        <img src="/port3.png" className='rounded-lg' alt="" />
                         <div className='absolute top-0 w-full rounded-lg dd flex  hover:bg-[#1abc9ce6] h-full justify-center items-center transition-all duration-500'>
                             <i className='fa-solid fa-plus text-8xl icon opacity-0 text-white'></i>
                         </div>
